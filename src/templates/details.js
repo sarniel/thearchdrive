1f939cca2bc9d097791ca38953bbb56b32fe0783
Sarniel Morgia <sarniel@Niels-MBP.gateway>
2024-01-04 15:43:03 +1100

    add manual ads

--- src/templates/details.js
+++ src/templates/details.js
@@ -119,6 +119,17 @@ const DetailPage = ({ data }) => {
                                                 ></Post>
                                             )
                                         )}
+                                        <amp-ad
+                                            width="100vw"
+                                            height="320"
+                                            type="adsense"
+                                            data-ad-client="ca-pub-2452081808089907"
+                                            data-ad-slot="3132984669"
+                                            data-auto-format="rspv"
+                                            data-full-width=""
+                                        >
+                                            <div overflow=""></div>
+                                        </amp-ad>
                                     </div>
                                 </div>
                             ) : null}
